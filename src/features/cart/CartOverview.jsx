da7fed3173e3e371c991f456780054aa312e8738
Mahmoud Mohsen <mahmoud.mohsen120@gmail.com>
2024-02-14 17:33:14 +0200

    adding tailwind and styling the home page

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div>
-      <p>
+    <div className=" flex justify-between bg-stone-800 px-4 py-4 uppercase text-slate-200 sm:px-6 sm:text-sm md:text-base">
+      <p className=" space-x-4 font-semibold text-slate-300 sm:space-x-6">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
